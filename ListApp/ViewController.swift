102f0229cf12159ad208a855c3a24a396e3e6271
hilal karatas <karataas.hilal@gmail.com>
2022-11-05 23:46:34 +0300

    Initial Commit

---
+++ ListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Hilal Karataş on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
